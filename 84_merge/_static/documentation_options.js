95e557e7803f20517bba7bc606d1bcef94111f34
Relm-Arrowny <Relm-Arrowny@users.noreply.github.com>
2025-04-17 11:45:53 +0000

    deploy: 7fdd3ba4857ee61054a542b75ee1f586947cc036

--- 84_merge/_static/documentation_options.js
+++ 84_merge/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.1.2.dev23+gcb7f84d',
+    VERSION: '0.1.1.2.dev24+g7fdd3ba',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
